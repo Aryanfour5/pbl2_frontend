26532f1b06a095877b584f38fff95aa0d9f77309
arryanfour5 <bachutearyan@gmail.com>
2025-05-03 00:14:43 +0530

    some UI Modifications

--- Backend/App.py
+++ Backend/App.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-CORS(app, origins=["https://pbl2-frontend.vercel.app/"])
+CORS(app, resources={r"/upload": {"origins": "https://pbl2-frontend.onrender.com"}})
 
 UPLOAD_FOLDER = "uploads"
 ALLOWED_EXTENSIONS = {"csv", "txt"}
